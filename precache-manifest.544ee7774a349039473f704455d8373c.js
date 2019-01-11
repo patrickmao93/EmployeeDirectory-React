18e6b147ae6156f691f3233767485b094376431b
Patrick <patrickmao93@gmail.com>
2019-01-11 13:23:47 +0800

    Updates

---
+++ precache-manifest.544ee7774a349039473f704455d8373c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31f27ebc82b376c24a70",
+    "url": "/EmployeeDirectory-React/static/js/runtime~main.31f27ebc.js"
+  },
+  {
+    "revision": "29f94f1e61a5cbf41f26",
+    "url": "/EmployeeDirectory-React/static/js/main.29f94f1e.chunk.js"
+  },
+  {
+    "revision": "648ea3c9f7f2b3781bd5",
+    "url": "/EmployeeDirectory-React/static/js/1.648ea3c9.chunk.js"
+  },
+  {
+    "revision": "29f94f1e61a5cbf41f26",
+    "url": "/EmployeeDirectory-React/static/css/main.12c30417.chunk.css"
+  },
+  {
+    "revision": "07a96e8b4f61c17f2c7936691f6694bb",
+    "url": "/EmployeeDirectory-React/index.html"
+  }
+];
